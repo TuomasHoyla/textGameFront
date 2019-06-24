b92a356803f6c26caa037371444108838a7d98c3
Tuomas Höylä <tuomas.hoyla@probissolutions.fi>
2019-06-24 11:17:45 +0300

    typeScript test/fix

--- src/Components/View.js
+++ src/Components/View.js
@@ -1,11 +1,18 @@
 "use strict";
-exports.__esModule = true;
-var React = require("react");
-var View = function (props) {
-    var data = props.data;
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __importStar(require("react"));
+const View = (props) => {
+    const { data } = props;
     return (React.createElement("div", null,
-        React.createElement("p", null, data.location && data.location.room && data.location.room.name),
-        React.createElement("p", null, data.location && data.location.room && data.location.room.text),
+        React.createElement("p", null, data.location && data.location.name),
+        React.createElement("p", null, data.location && data.location.text),
         React.createElement("p", { className: "App-link underLine" }, data.message)));
 };
-exports["default"] = View;
+exports.default = View;
--- src/Components/View.tsx
+++ src/Components/View.tsx
@@ -4,10 +4,8 @@ type ViewType = {
     data: {
       message : string
       location : {
-        room : {
           name : string
           text : string
-        }
       }
     } 
 }
@@ -18,15 +16,15 @@ const View: React.SFC<ViewType> = (props) => {
 
   return (
   <div>
-    <p>
-    {data.location && data.location.room.name}
-    </p>
-    <p>
-    {data.location && data.location.room.text}
-    </p>
-    <p className="App-link underLine">
-    {data.message}
-    </p>
+      <p>
+        {data.location && data.location.name}
+      </p>
+      <p>
+        {data.location && data.location.text}
+      </p>
+      <p className="App-link underLine">
+        {data.message}
+      </p>
   </div>
   )
 }
